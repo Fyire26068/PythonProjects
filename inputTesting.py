a4a709588dcc831ebfb9132dfcb94957d783659a
Anthony Garrard <anthony.garrard@tooeleschools.org>
2020-09-17 10:03:36 -0600

    edited files for successful push

---
+++ inputTesting.py
@@ -0,0 +1,5 @@
+username = str #testing input function
+
+username = input("Please enter username : ")
+print("Username", username, sep=" : ")
+#comment for git push
---
+++ test.py
@@ -0,0 +1,15 @@
+x=3
+print(type(x))
+x = x+0.8
+
+print(type(x))
+
+name = "Anthony"
+print(type(name))
+
+awake = False
+print(type(awake))
+
+print\
+(x)
+#comment for push
